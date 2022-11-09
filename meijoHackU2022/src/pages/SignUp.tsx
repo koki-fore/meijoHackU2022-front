91468a235d737e75023c6e0d9cdf49866a1b0a85
koki-fore <furukunn@gmail.com>
2022-11-09 11:57:05 +0900

    login画面とサインアップ画面を作成

--- meijoHackU2022/src/pages/SignUp.tsx
+++ meijoHackU2022/src/pages/SignUp.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import { Heading, Input, Text } from '@chakra-ui/react'
+import { Heading, Input, InputGroup, Text } from '@chakra-ui/react'
 import PasswordInput from '../components/PasswordInput'
+import { Button } from '@chakra-ui/react'
 
 const SignUp: React.FC = () => {
     return (
         <>
             <Heading>ユーザー登録</Heading>
-            <Text>メールアドレス</Text>
-            <Input placeholder='aaaa@gmail.com' />
-            <Text>パスワード</Text>
-            <PasswordInput />
+            <InputGroup>
+                <Text>メールアドレス</Text>
+                <Input placeholder='aaaa@gmail.com' />
+                <Text>パスワード</Text>
+                <PasswordInput />
+                <Button colorScheme='teal' variant='solid' >
+                    サインアップ
+                </Button>
+            </InputGroup>
         </>
     )
 }; export default SignUp;
